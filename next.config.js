fe8c73c70d1090fb1e70a971b904ec25fa3e0b58
Artur Ch <chmarus@gmail.com>
2023-08-25 22:00:19 +0200

    Readme and config update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  typescript: {
+    // !! just a demo !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! just a demo !!
+    ignoreBuildErrors: true,
+  },
+}
 
 module.exports = nextConfig
